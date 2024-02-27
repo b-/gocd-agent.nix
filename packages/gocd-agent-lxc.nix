25c2acf021e499e281163f0fd3b0c0ab92703cb5
bri <bri@bri-macbook>
2024-02-27 15:00:13 -0500

    update flake.lock

--- packages/gocd-agent-lxc.nix
+++ packages/gocd-agent-lxc.nix
@@ -9,13 +9,13 @@ let
       ...
     }:
     {
+      disabledModules = [ "${modulesPath}/services/continuous-integration/gocd-agent/default.nix" ];
+      imports = [ "${inputs.s}/nixos/modules/services/continuous-integration/gocd-agent/default.nix" ];
       config = {
         services.gocd-agent = {
           enable = true;
           goServer = "http://192.168.30.40:8153/go";
         };
-          disabledModules = [ "${modulesPath}/services/continuous-integration/gocd-agent/default.nix" ];
-          imports = [ "${inputs.s}/nixos/modules/services/continuous-integration/gocd-agent/default.nix" ];
       };
     };
 in
