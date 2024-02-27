ddf5f8a0ca5546172e40a54396c2f9585fd4887d
bri <bri@bri-macbook>
2024-02-27 14:53:05 -0500

    update flake.lock

--- packages/gocd-agent-lxc.nix
+++ packages/gocd-agent-lxc.nix
@@ -1,22 +1,26 @@
 { inputs, ... }@flakeContext:
 let
-  nixosModule = { config, lib, pkgs, self, modulesPath, ... }: {
-    config = {
-      disabledModules = [ "${modulesPath}/services/continuous-integration/gocd-agent/default.nix" ];
-      imports = [ "${inputs.s}/nixos/modules/services/continuous-integration/gocd-agent/default.nix" ];
-      services = {
-        gocd-agent = {
+  nixosModule =
+    {
+      config,
+      lib,
+      modulesPath,
+      pkgs,
+      ...
+    }:
+    {
+      config = {
+        services.gocd-agent = {
           enable = true;
           goServer = "http://192.168.30.40:8153/go";
         };
+          disabledModules = [ "${modulesPath}/services/continuous-integration/gocd-agent/default.nix" ];
+          imports = [ "${inputs.s}/nixos/modules/services/continuous-integration/gocd-agent/default.nix" ];
       };
     };
-  };
 in
 inputs.nixos-generators.nixosGenerate {
   system = "x86_64-linux";
   format = "proxmox-lxc";
-  modules = [
-    nixosModule
-  ];
+  modules = [ nixosModule ];
 }
